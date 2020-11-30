0da6823467e00b8b35b51c81eff81fab255a78be
lewinttzz <lewin.buttazzo@gmail.com>
2020-11-30 11:42:07 +0100

    Update admin.py

--- mychores/admin.py
+++ mychores/admin.py
@@ -3,6 +3,8 @@ from .models import Task,Task_Instance, Profile
 
 # Register your models here.
 admin.site.register(Task)
-admin.site.register(Task_Instance)
 admin.site.register(Profile)
 
+@admin.register(Task_Instance)
+class TaskInAdmin(admin.ModelAdmin):
+    list_display = ('taskTbd','dueDate', 'state')
